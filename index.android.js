8abf698e97e33e8524bbdbba7ac6edb94603095b
Matthew Roseman <mroseman95@gmail.com>
2017-05-16 16:20:58 -0400

    began working on sign in page. So far is it very basic and just has elements on it. Need to align better
    Signed-off-by: Matthew Roseman <mroseman95@gmail.com>

--- index.android.js
+++ index.android.js
@@ -1,53 +1,174 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    AppRegistry,
+    StyleSheet,
+    Text,
+    TextInput,
+    Image,
+    View,
+    Button,
 } from 'react-native';
+import {
+    StackNavigator,
+} from 'react-navigation';
 
-export default class OnlineGoApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
-    );
-  }
+class SignInFields extends Component {
+    render() {
+        return (
+            <View>
+                <TextInput style={{height: 40, width: 300}} placeholder="Username or Email"
+                    onChangeText={(text) => this.setState({text})}/>
+                <TextInput style={{height: 40, width: 300}} placeholder="Password"
+                    onChangeText={(text) => this.setState({text})}/>
+            </View>
+        );
+    }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+class AltSignInOptions extends Component {
+    render() {
+        return (
+            <View style={{flexDirection: 'row'}}>
+                <Text>Facebook logo</Text>
+                <Text>Google logo</Text>
+                <Text>Twitter logo</Text>
+            </View>
+        );
+    }
+}
+
+class HomeScreen extends Component {
+    static navigationOptions = {
+        title: 'online-go.com',
+    };
+
+    render() {
+        const { navigate } = this.props.navigation;
+        return (
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
+                <View style={{flex: 1, height: 100, width: 250, justifyContent: 'center', paddingVertical: 20}}>
+                    <Button onPress={ () => navigate('SignIn') } 
+                        title="Sign In"/>
+                </View>
+                <View style={{flex: 6}}/>
+            </View>
+        );
+    }
+}
+
+class SignInScreen extends Component {
+    static navigationOptions = () => ({
+        title: 'Sign In',
+    });
+
+    render() {
+        const { params } = this.props.navigation.state;
+        // TODO 
+        // put the sign in stuff in a box with a shadow
+        // below the box have a register and forgot password option
+        return (
+            <View style={{flex: 1, flexDirection: 'column', alignItems: 'center'}}>
+                <View style={{flex: 1, paddingVertical: 20}}>
+                    <SignInFields/>
+                </View>
+                <View style={{flex: 1, width: 250, paddingVertical: 20}}>
+                    <Button onPress={ () => {} } title="Sign In"/>
+                </View>
+                <View style={{flex: 1, paddingVertical: 20}}>
+                    <Text>or sign in with</Text>
+                </View>
+                <View style={{flex: 1, paddingVertical: 20}}>
+                    <AltSignInOptions/>
+                </View>
+            </View>
+        );
+    }
+}
+
+const OnlineGoApp = StackNavigator ({
+    Home: { screen: HomeScreen },
+    SignIn: { screen: SignInScreen },
 });
 
 AppRegistry.registerComponent('OnlineGoApp', () => OnlineGoApp);
+
+// const styles = StyleSheet.create({
+//     smallred: {
+//         color: 'red',
+//         fontSize: 10
+//     },
+//     bigblue: {
+//         color: 'blue',
+//         fontWeight: 'bold',
+//         fontSize: 30
+//     }
+// });
+// 
+// class FlexDirectionBasics extends React.Component {
+//     render() {
+//         return (
+//             <View style={{flex: 1, flexDirection: 'row'}}>
+//               <View style={{width: 50, height: 50, backgroundColor: 'powderblue'}}/>
+//               <View style={{width: 50, height: 50, backgroundColor: 'skyblue'}}/>
+//               <View style={{width: 50, height: 50, backgroundColor: 'steelblue'}}/>
+//             </View>
+//         );
+//     }
+// }
+// 
+// class Blink extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         // Start off True
+//         this.state = {showText: true};
+// 
+//         // Run the function passed in as first argument every 1000 milliseconds
+//         setInterval(() => {
+//             // setState calls the render function again
+//             this.setState({ showText: !this.state.showText });
+//         }, 1000);
+//     }
+// 
+//     render() {
+//         // Check what the state is every time this renders
+//         let display = this.state.showText ? this.props.text : ' ';
+//         return (
+//             <Text>{display}</Text>
+//         );
+//     }
+// }
+// 
+// class Greeting extends React.Component {
+//     render() {
+//         return (
+//             <Text>Hello {this.props.name}!</Text>
+//         )
+//     }
+// }
+// 
+// class OnlineGoApp extends React.Component {
+//     render() {
+//         let pic = {
+//             uri: 'http://c10.nrostatic.com/sites/default/files/styles/original_image_with_cropping/public/uploaded/donald-trump-grow-up.jpg?itok=n1PW3Myr'
+//         };
+//         return (
+//             <View style={{flex: 1}}>
+//               <View style={{alignItems: 'center'}}>
+//                 <Text>Hello world!</Text>
+//                 <Image source={pic} style={{width: 193, height: 110}}/>
+//                 <Greeting name='Matthew'/>
+//                 <Blink text='This is blinking text!'/>
+//                 <Text style={styles.smallred}>small red text</Text>
+//                 <Text style={[styles.smallred, styles.bigblue]}>BIG BLUE TEXT</Text>
+//               </View>
+//               <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
+//               <View style={{flex: 2}}>
+//                 <FlexDirectionBasics/>
+//               </View>
+//               <View style={{flex: 3, backgroundColor: 'steelblue'}}/>
+//             </View>
+//         );
+//     }
+// }
+// 
+// AppRegistry.registerComponent('OnlineGoApp', () => OnlineGoApp);
